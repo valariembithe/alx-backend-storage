a0d0b5633ce25bc53b07d59421ae8447964bbbf1
valariembithe <valmbithe691@gmail.com>
2024-01-18 03:00:22 -0800

    Task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- create a table named users
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN('US', 'CO', 'TN'))
+);
