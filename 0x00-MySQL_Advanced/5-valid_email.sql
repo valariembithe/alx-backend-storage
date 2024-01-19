36561aa316884b4e04b17686ec5b5b5f8e08755f
valariembithe <valmbithe691@gmail.com>
2024-01-19 03:34:14 -0800

    Tasks

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Task 5 module
+DROP TRIGGER IF EXISTS validate_email;
+DELIMITER $$
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF OLD.email != NEW.email THEN
+		SET NEW.valid_email = 0
+	ELSE
+		SET NEW.valid_email = NEW.valid_email
+	END IF;
+END $$
+DELIMITER;
