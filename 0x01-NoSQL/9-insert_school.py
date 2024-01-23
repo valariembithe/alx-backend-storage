c84846eba19c000cbc3714b78a20ad012992653f
valariembithe <valmbithe691@gmail.com>
2024-01-23 02:01:13 -0800

    Tasks

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,6 +4,6 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection """
-    result = mongo_collection.insert_one(**kwargs)
+    result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
 
