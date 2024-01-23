d6da50cc0b118fbfd9ddc562342f8405b1ad8c28
valariembithe <valmbithe691@gmail.com>
2024-01-23 01:43:23 -0800

    Tasks

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" List all documents in Python"""
+
+
+def list_all(mongo_collection):
+    """ Lists all documents in a collection """
+    return [doc for doc in mongo_collection.find()]
+
